6dd33b277016c697f23f956d39e37caffe1df723
Roberto Bocio Melo <robertobocio@01lab.co>
2022-05-11 10:16:25 -0500

    [FIX] vars railway port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PGPORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
