eeddcd73335c63a7d2141c1dee7ebcacbb2ac32f
Roberto Bocio Melo <robertobocio@01lab.co>
2022-05-11 19:48:18 -0500

    re back

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import express from "express";
+import app from "./app";
 
-const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(express.json());
-// app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-// app.use(bodyParser.text({ type: "text/html" }));
-
-app.get("/", async (req, res) => {
-  res.json({ Hello: "World" });
-});
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`listening at http://localhost:${port}`);
 });
-
