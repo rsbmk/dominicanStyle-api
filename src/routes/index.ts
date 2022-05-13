89ec1f9d8a6b2d93380cec99b2b9194522e3ce4d
Roberto Bocio Melo <robertobocio@01lab.co>
2022-05-13 02:09:53 -0500

    [ADD] new endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { appointmentRouter } from "./appointment";
 import { employeesRouter } from "./employees";
+import { serviceRoute } from "./services";
 import { teamRoute } from "./team";
 
 export const router = Router();
 
-router.use("/v1/employees", employeesRouter);
-router.use("/v1/team", teamRoute);
 router.use("/v1/appointment", appointmentRouter);
+router.use("/v1/employees", employeesRouter);
+router.use("/v1/service", serviceRoute);
+router.use("/v1/team", teamRoute);
