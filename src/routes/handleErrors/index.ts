97726b6173ed8ed17cdc4da2cd64a29c5a335e76
Roberto Bocio Melo <robertobocio@01lab.co>
2022-05-29 16:59:14 -0500

    [REFACTOR] add func validations

--- src/routes/handleErrors/index.ts
+++ src/routes/handleErrors/index.ts
@@ -9,3 +9,15 @@ export const errorRouter = (request: Request, response: Response) => {
     })
     .end();
 };
+
+export const handleCatch = (error: any, response: Response) => {
+  return response
+    .status(500)
+    .json({
+      message: error.meta?.cause ?? error,
+      status: 400,
+      error: error.message,
+      primaError: error,
+    })
+    .end();
+};
