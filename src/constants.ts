2d664286fffe568ec7ee55780a469841121b53b9
Roberto Bocio Melo <robertobocio@01lab.co>
2022-08-20 20:37:46 -0500

    [ADD] constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,14 @@
-import dayjs from "dayjs";
-
 export const PERIODS = {
-  day: {
-    start: dayjs().startOf("day").toJSON(),
-    end: dayjs().endOf("day").toJSON(),
-  },
-  week: {
-    start: dayjs().startOf("week").toJSON(),
-    end: dayjs().endOf("week").toJSON(),
-  },
-  month: {
-    start: dayjs().startOf("month").toJSON(),
-    end: dayjs().endOf("month").toJSON(),
-  },
-  year: {
-    start: dayjs().startOf("year").toJSON(),
-    end: dayjs().endOf("year").toJSON(),
-  },
+  day: "day",
+  week: "week",
+  month: "month",
+  year: "year",
+};
+
+export const APPOINTMENT_STATUS = {
+  PENDING: "pending",
+  CONFIRMED: "confirmed",
+  PROCESS: "process",
+  CLOSE: "close",
+  CANCELLED: "cancelled",
 };
