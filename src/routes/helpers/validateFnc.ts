a7df34dd9be18f1fb525bf0fa391e396a8aac822
Roberto Bocio Melo <robertobocio@01lab.co>
2022-08-20 20:40:46 -0500

    [FIX & ADD] validations

--- src/routes/helpers/validateFnc.ts
+++ src/routes/helpers/validateFnc.ts
@@ -2,9 +2,10 @@ import { Response } from "express";
 import dayjs from "dayjs";
 
 export function validateManyFilds(response: Response, objToValidate: any, fields: string[]) {
+  let errors = false;
   fields.forEach((field) => {
     if (!objToValidate[field]) {
-      return response
+      response
         .status(400)
         .json({
           error: "Todos los campos son requeridos",
@@ -14,27 +15,33 @@ export function validateManyFilds(response: Response, objToValidate: any, fields
           status: 400,
         })
         .end();
+      errors = true;
     }
   });
+  return errors;
 }
 
 export function validateNumberFild(response: Response, field: string, value: number) {
-  if (!value || isNaN(value))
-    return response
+  if (isNaN(value) || typeof value !== "number") {
+    response
       .status(400)
       .json({
-        error: "El campo es requerido",
-        status: 400,
-        message: "Este campo es requerido",
+        error: "El campo debe ser un numero",
+        fields: [field],
+        message: "Este campo debe ser un numero",
         nameInput: field,
+        status: 400,
       })
       .end();
+    return true;
+  }
+  return false;
 }
 
 export function validateTelephone(response: Response, telephone: string) {
   const validateNumber = /^09[0-9]{8}$/;
-  if (!validateNumber.test(telephone))
-    return response
+  if (!validateNumber.test(telephone)) {
+    response
       .status(400)
       .json({
         error: "El número de teléfono no es valido",
@@ -44,11 +51,14 @@ export function validateTelephone(response: Response, telephone: string) {
         status: 400,
       })
       .end();
+    return true;
+  }
+  return false;
 }
 
 export function validateIsDate(response: Response, date: Date) {
-  if (!dayjs(date).isValid())
-    return response
+  if (!dayjs(date).isValid()) {
+    response
       .status(400)
       .json({
         error: "La fecha no es valida",
@@ -58,4 +68,87 @@ export function validateIsDate(response: Response, date: Date) {
         status: 400,
       })
       .end();
+    return true;
+  }
+  return false;
 }
+
+export function validateArray(response: Response, array: any[], field: string) {
+  if (!Array.isArray(array)) {
+    response
+      .status(400)
+      .json({
+        error: "El campo debe ser un array",
+        fields: [field],
+        message: "Este campo debe ser un array",
+        nameInput: field,
+        status: 400,
+      })
+      .end();
+    return true;
+  }
+
+  if (array.length === 0) {
+    response
+      .status(400)
+      .json({
+        error: "El campo debe tener al menos un elemento",
+        fields: [field],
+        message: "Este campo debe tener al menos un elemento",
+        nameInput: field,
+        status: 400,
+      })
+      .end();
+    return true;
+  }
+
+  return false;
+}
+
+export const isntAfterNow = (response: Response, date: Date) => {
+  let message = "No puede crear una cita con una fecha u hora anterior a la actual";
+
+  const minTimeForAppointment = 3; // minimum time for an appointment in hours
+  const itIsAfterNow = dayjs(date).isAfter(dayjs(), "second");
+  const isAfterAtMinTime = dayjs(date).isAfter(dayjs().add(minTimeForAppointment, "hour"));
+
+  const doesNotMinTime = itIsAfterNow && isAfterAtMinTime === false;
+
+  if (itIsAfterNow && doesNotMinTime) {
+    //does not respect the minimum hours of anticipation
+    message = "No se puede agendar una cita sin el tiempo mínimo de anticipación";
+  }
+
+  if (itIsAfterNow === false || doesNotMinTime)
+    response
+      .status(400)
+      .json({
+        message,
+        status: 400,
+        error: "Fecha invalida",
+        fields: ["appointmentDate"],
+        nameInput: "appointmentDate",
+      })
+      .end();
+
+  return itIsAfterNow === false || doesNotMinTime;
+};
+
+//minimun length for a CI is 10
+export const isCIValid = (response: Response, ci: string, field: string) => {
+  const validateCi = /^[0-9]{10}$/;
+  if (!validateCi.test(ci)) {
+    response
+      .status(400)
+      .json({
+        error: "La cedula no es valida",
+        fields: [field],
+        message: "Debe tener 10 digitos",
+        nameInput: field,
+        status: 400,
+      })
+      .end();
+    return true;
+  }
+  return false;
+};
